dbe9c98125afcea91cfee26191088ba39f26d7aa
Pablo Gallego Carmona <pablogallegocarmona@gmail.com>
2022-09-05 16:25:06 +0200

    cambios en la cabecera

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 const Navbar = ({}) => {
-  return <div>NavBar para el portafolio de kalpa</div>
+  return <div>NavBar para el portafolio de kalpa y probando</div>
 }
 
 export default Navbar
