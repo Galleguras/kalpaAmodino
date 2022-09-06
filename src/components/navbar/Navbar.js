69b72a527579457afa4bae7b486adc12944f754b
Pablo Gallego Carmona <pablogallegocarmona@gmail.com>
2022-09-06 10:39:51 +0200

    ssh

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,7 +1,11 @@
 import React from "react"
 
 const Navbar = ({}) => {
-  return <div>NavBar para el portafolio de kalpa y probando otra vez</div>
+  return (
+    <div>
+      NavBar para el portafolio de kalpa y probando otra vez con la conexion ssh
+    </div>
+  )
 }
 
 export default Navbar
