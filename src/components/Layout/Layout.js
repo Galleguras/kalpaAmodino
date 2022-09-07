aaa99f24d77ece29f058c40bd768f734a951f768
Pablo Gallego Carmona <pablogallegocarmona@gmail.com>
2022-09-07 15:41:42 +0200

    contenedor

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,22 @@
 import React, { useEffect, useState } from "react"
 import Footer from "../footer"
 import Navbar from "../navbar/Navbar"
+import { Container, Box } from "@mui/material"
 const Layout = ({ uri, children }) => {
   return (
-    <>
-      <Navbar />
+    <Container
+      maxWidth={false}
+      disableGutters="true"
+      sx={{
+        background: "whitesmoke",
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
+      {/*    <Navbar /> */}
       {children}
-      <Footer />
-    </>
+      {/*   <Footer /> */}
+    </Container>
   )
 }
 
